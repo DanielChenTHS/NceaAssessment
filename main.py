2221fe3c4298f759d85a396aeeb5269c7eee41d9
Daniel Chen <dc17088@thameshigh.school.nz>
2022-08-20 13:29:29 +1200

    Adding in the basic sprites, movement / gravity implemented.
    About to resize images to better match game

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import mc_pgzrun
-from pgzero.game import screen
-from pgzero.keyboard import keyboard
-
 from pgzhelper import *
+import random
 
 WIDTH = 564
 HEIGHT = 764
 
 player = Actor('player')
-player.x = 370
-player.y = 550
+player.x = 282
+player.y = 724
+
+can = Actor('can')
+can.x = random.randint(20,544)
+can.y = 0
+
 
 def update():
     if keyboard.left:
@@ -17,10 +20,19 @@ def update():
     if keyboard.right:
         player.x = player.x + 5
 
-def draw(background='background'):
-    screen.blit(background, (0, 0))
+    can.y = can.y + 4
+    if can.y > 764:
+        can.x = random.randint(20,544)
+        can.y = 0
+
+    if can.colliderect(player):
+        can.x = random.randint(20, 544)
+        can.y = 0
+
+
+def draw():
+    screen.blit('background', (0, 0))
     player.draw()
+    can.draw()
 
-
-
-mc_pgzrun.go()
+mc_pgzrun.go() # Must be last line
